d558f693a057d38093d3b69dcf4c2e83727c5118
toobut <toobut75009@gmail.com>
2020-07-17 20:10:42 +0800

    20200717test

---
+++ ZMLis/ZMLis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZMLis.MvcApplication" Language="C#" %>
---
+++ ZMLis/ZMLis/ZMLisAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ZMCMSv2.Areas.ZMLis
+{
+    public class ZMLisAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ZMLis";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ZMLis_default",
+                "ZMLis/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
